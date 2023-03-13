b1fece7a51a3f8d6ae2b400f3f2461b1f3432add
jonasporpinoprof <105394156+jonasporpinoprof@users.noreply.github.com>
2023-03-13 19:42:45 -0300

    Primeiro

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function verde(){
+  luzAmarela.style.backgroundColor = "#771";
+  luzVermelha.style.backgroundColor = "#912";
+  /**luz principal */
+  luzVerde.style.backgroundColor = "#0f1";
+}
+
+function amarelo(){
+  luzVerde.style.backgroundColor = "#070";
+  luzVermelha.style.backgroundColor = "#912";
+  /**luz principal */
+  luzAmarela.style.backgroundColor = "#ff0";
+}
+function vermelho(){
+  luzAmarela.style.backgroundColor = "#771";
+  luzVerde.style.backgroundColor = "#070";
+  /**luz principal */
+  luzVermelha.style.backgroundColor = "#f00";
+}
